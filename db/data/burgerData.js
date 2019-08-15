8071fb02b6f77d264e75fbf80156d4ea40da9b25
Chris Beaney <chrisbeaney@gmail.com>
2019-08-15 15:13:41 +0100

    added a burger

---
+++ db/data/burgerData.js
@@ -0,0 +1,18 @@
+module.exports = [{
+  name: 'Hawksmoor Hamburger',
+  rating: 4.5,
+  image: 'https://secure.i.telegraph.co.uk/multimedia/archive/03419/burgerhawk_3419434b.jpg',
+  ingredients: [
+    'beef',
+    'bone marrow'
+  ],
+  price: 17.00,
+  description: 'Consisting of of 100% Longhorn, it includes old fashioned cuts like Clod and Sticking and small nuggets of bone marrow.',
+  restaurant: {
+    name: 'The Hawksmoor',
+    address: '5A Air Street, London W1J 0AD',
+    latitude: 51.5097213,
+    longitude: -0.1383511,
+    website: 'https://thehawksmoor.com/'
+  }
+}]
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Home = () => {
-<section className='hero is-small'>
-  <div className="hero-body">
-    <div className="container">
-      <h1 className="title">Burgerator</h1>
-      <h2 className="subtitle">Burgers Rated by YOU</h2>
+  <section className='hero is-small'>
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title">Burgerator</h1>
+        <h2 className="subtitle">Burgers Rated by YOU</h2>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 
 }
 
