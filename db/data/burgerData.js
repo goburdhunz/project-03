e4512b7dd7df13f2039040857c6b0e9db18fa962
Chris Beaney <chrisbeaney@gmail.com>
2019-08-15 20:34:18 +0100

    added burgers

--- db/data/burgerData.js
+++ db/data/burgerData.js
@@ -5,16 +5,90 @@ module.exports = [{
   image: 'https://secure.i.telegraph.co.uk/multimedia/archive/03419/burgerhawk_3419434b.jpg',
   ingredients: [
     'beef',
-    'bone marrow'
+    'bone marrow',
+    'Ogleshield cheese',
+    'pickle',
+    'onion',
+    'lettuce'
   ],
   price: 17.00,
-  description: 'Consisting of of 100% Longhorn, it includes old fashioned cuts like Clod and Sticking and small nuggets of bone marrow.',
+  description: 'Consisting of of 100% Longhorn beef, it includes old fashioned cuts like Clod and Sticking and small nuggets of bone marrow.',
   restaurant: {
     name: 'The Hawksmoor',
     address: '5A Air Street, London W1J 0AD',
     latitude: 51.5097213,
     longitude: -0.1383511,
     website: 'https://thehawksmoor.com/'
+  }
+},
+
+{
+  name: 'Cheeseburger',
+  rating: 4,
+  image: 'https://burgerandbeyond.co.uk/wp-content/uploads/2018/11/1.jpg',
+  ingredients: [
+    'Beef',
+    'American cheese',
+    'Smoky Mayo',
+    'Onion'
+  ],
+  price: 12.00,
+  description: 'Our signature Dry Aged Beef Patty, Double American Cheese, Smoky Mayo and Onion',
+  restaurant: {
+    name: 'Burger & Beyond',
+    address: '47 Shoreditch High Street, London, E1 6JE',
+    latitude: 51.5256932,
+    longitude: -0.0796717,
+    website: 'https://burgerandbeyond.co.uk'
+  }
+},
+
+{
+  name: 'Bacon Double Cheeseburger',
+  rating: 3.5,
+  image: 'https://scontent.cdninstagram.com/vp/fb5bd7a391114e2c7bbbd92af6404a38/5DDFD734/t51.2885-15/sh0.08/e35/p640x640/67330224_708696306259691_778761288439457175_n.jpg?_nc_ht=scontent.cdninstagram.com',
+  ingredients: [
+    'beef',
+    'cheese',
+    'onion',
+    'bacon',
+    'house sauce'
+  ],
+  price: 11.00,
+  description: 'Double beef patty, double cheese, onion, bacon & house sauce.',
+  restaurant: {
+    name: 'bleeker',
+    address: 'Old Spitalfields Market, Unit B SP 4, London E1 6EA',
+    latitude: 51.5185314,
+    longitude: -0.0766692,
+    website: 'bleeker.co.uk'
+  }
+},
+
+{
+  name: 'The Samuel Hell Jackson',
+  rating: 4,
+  image: 'https://f3e6t7k9.stackpathcdn.com/wp-content/uploads/2018/10/biffsjackshack-loti.jpg?x66545',
+  ingredients: [
+    'jackfruit',
+    'brioche',
+    'onion',
+    'carrot',
+    'jalapeños',
+    'American cheese',
+    'BBQ sauce',
+    'hot sauce'
+  ],
+  price: 10.00,
+  description: 'Vegan crispy fried jackfruit burgers, Chipotle slaw, maple chipotle hot sauce, jalapeños, american cheeze, BBQ sauce in a toasted brioche bun',
+  isVegan: true,
+  isVegetarian: true,
+  restaurant: {
+    name: 'Biff\'s Jack Shack',
+    address: '',
+    latitude: 51.5234906,
+    longitude: -0.0785956,
+    website: 'https://biffsjackshack.com'
   }
 },
 
