971ccdca5593306d0898ccf80b7c2b0c013e803b
Zeeshan Goburdhun <goburdhunz@hotmail.co.uk>
2019-08-15 16:47:13 +0100

    created home route with changes to route, pages files and burgerdata

--- db/data/burgerData.js
+++ db/data/burgerData.js
@@ -25,8 +25,6 @@ module.exports = [{
   ingredients: ['Bacon', 'Tomato', 'Bbq sauce', 'Beef', 'Lettuce', 'Bun' ],
   price: 1100,
   description: 'Big juicy patty smothered in bbq sauce with smoked bacon',
-  isVegan: {type: Boolean, default: false},
-  isVegetarian: {type: Boolean, default: false},
   restaurant: {
     name: 'Bucks',
     address: '12 Hamburg lane, Hamburg',
@@ -42,8 +40,36 @@ module.exports = [{
   ingredients: ['Bacon', 'Tomato', 'Bbq sauce', 'Beef', 'Lettuce', 'Bun' ],
   price: 1100,
   description: 'Big juicy patty smothered in bbq sauce with smoked bacon',
-  isVegan: {type: Boolean, default: false},
-  isVegetarian: {type: Boolean, default: false},
+  restaurant: {
+    name: 'Bucks',
+    address: '12 Hamburg lane, Hamburg',
+    latitude: 53.5533688,
+    longitude: 10.0028873,
+    website: 'https://www.bucks-burgers.de/'
+  }
+},
+{
+  name: 'BBQ Bacon Burger',
+  rating: 2,
+  image: 'https://files.webbuilder.hosteurope.de/eb/9f/eb9f74db-a75a-4921-8e43-ce03e0c024d8.JPG',
+  ingredients: ['Bacon', 'Tomato', 'Bbq sauce', 'Beef', 'Lettuce', 'Bun' ],
+  price: 1100,
+  description: 'Big juicy patty smothered in bbq sauce with smoked bacon',
+  restaurant: {
+    name: 'Bucks',
+    address: '12 Hamburg lane, Hamburg',
+    latitude: 53.5533688,
+    longitude: 10.0028873,
+    website: 'https://www.bucks-burgers.de/'
+  }
+},
+{
+  name: 'BBQ Bacon Burger',
+  rating: 3,
+  image: 'https://files.webbuilder.hosteurope.de/eb/9f/eb9f74db-a75a-4921-8e43-ce03e0c024d8.JPG',
+  ingredients: ['Bacon', 'Tomato', 'Bbq sauce', 'Beef', 'Lettuce', 'Bun' ],
+  price: 1100,
+  description: 'Big juicy patty smothered in bbq sauce with smoked bacon',
   restaurant: {
     name: 'Bucks',
     address: '12 Hamburg lane, Hamburg',
