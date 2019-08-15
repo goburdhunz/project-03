c883fd409d9e7cdc4a29298f0e71cfa42f3daa0b
camilabuenamar <camila.buenamar@gmail.com>
2019-08-15 15:39:55 +0100

    Not sure why there is a card here...

---
+++ src/components/burgers/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = ({ name, image, restaurant, rating }) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+          <img src={image} alt={name} />
+        </figure>
+      </div>
+      <div className="card-header">
+        <div className="card-header-title">{name}</div>
+      </div>
+      <div className="card-content">
+        <h2 className="content">{rating} 🍔🍔🍔🍔🍔</h2>
+        <h2 className="content">{restaurant.name}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Card
