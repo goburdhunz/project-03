150749b0e03418e1f56a441dd05ec8e05c5879a4
Zeeshan Goburdhun <goburdhunz@hotmail.co.uk>
2019-08-16 13:52:40 +0100

    query and error handling added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const {dbURI} = require('./config/environment')
 const router = require('./config/routes')
+const queryHandler = require('./lib/queryHandler')
+const errorHandler = require('./lib/errorHandler')
 
 
 
@@ -13,8 +15,11 @@ mongoose.connect(dbURI, { useNewUrlParser: true })
 
 app.use(bodyParser.json())
 
+app.user(queryHandler)
 
 app.use('/api',router)
+
+app.user(errorHandler)
 
 
 app.listen(4000, () => console.log('Change made on port 4000'))
