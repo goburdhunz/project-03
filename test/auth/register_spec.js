03d7ae8988ab6abca34714736639b36cccde9ac3
Chris Beaney <chrisbeaney@gmail.com>
2019-08-16 11:28:45 +0100

    added register test

---
+++ test/auth/register_spec.js
@@ -0,0 +1,45 @@
+/* global api, describe, it, expect, afterEach */
+const User = require('../../models/User')
+
+const testData = {
+  username: 'testname',
+  email: 'testname@test.com',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}
+
+describe('POST /register', () => {
+
+  afterEach(done => {
+    User.remove({})
+      .then(() => done())
+  })
+
+  it('should return a 200 response', done => {
+    api.post('/api/register')
+      .send(testData)
+      .end((err, res) => {
+        expect(res.status).to.eq(200)
+        done()
+      })
+  })
+
+  it('should return an object', done => {
+    api.post('api/register')
+      .send(testData)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object')
+        done()
+      })
+  })
+
+  it('should return a message', done => {
+    api.post('/api/register')
+      .send(testData)
+      .end((err, res) => {
+        expect(res.body.message).to.eq('Registration successful')
+        done()
+      })
+  })
+
+})
