e6195388e55f5cff274fd9c1c38d937fffa9933d
camilabuenamar <camila.buenamar@gmail.com>
2019-08-19 10:54:48 +0100

    Setting up app for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
+const port = process.env.PORT || 4000
 const env = process.env.NODE_ENV || 'development'
-const dbURI = `mongodb://localhost:27017/burgerator-db-${env}`
-const secret = 'N&nd05_15_TH3+I3e5t'
+const dbURI = process.env.MONGODB_URI ||`mongodb://localhost:27017/burgerator-db-${env}`
+const secret = process.env.SECRET || 'N&nd05_15_TH3+I3e5t'
 
-module.exports = { dbURI, secret }
+module.exports = { port, env, dbURI, secret }
