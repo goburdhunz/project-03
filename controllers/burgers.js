0b9e0e254033337b1972114744ae2c39c801fd2f
camilabuenamar <camila.buenamar@gmail.com>
2019-08-19 14:07:35 +0100

    Created the route in burger controller

--- controllers/burgers.js
+++ controllers/burgers.js
@@ -7,6 +7,16 @@ function indexRoute(req,res, next) {
     .catch(next)
 }
 
+function showRoute(req, res, next) {
+  Burger.findById(req.params.id)
+    .populate({ path: 'user', select: '-email' })
+    .populate({ path: 'comments.user', select: '-email' })
+    .then(burger => {
+      if(!burger) return res.sendStatus(404)
+      return res.json(burger)
+    })
+    .catch(next)
+}
 
 function createRoute(req, res, next) {
   req.body.user = req.currentUser._id
@@ -19,5 +29,6 @@ function createRoute(req, res, next) {
 
 module.exports = {
   index: indexRoute,
-  create: createRoute
+  create: createRoute,
+  show: showRoute
 }
