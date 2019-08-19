bac909b1db547bbe9ed1f4b3881bc41d10f7e698
Zeeshan Goburdhun <goburdhunz@hotmail.co.uk>
2019-08-19 16:23:07 +0100

    added links to home page burgers

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from '../burgers/Card'
 import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 class Home extends React.Component {
   constructor() {
@@ -31,12 +32,14 @@ class Home extends React.Component {
                     key={burger._id}
                     className="column is-half-tablet is-one-third-desktop"
                   >
-                    <Card
-                      name={burger.name}
-                      image={burger.image}
-                      rating={burger.rating}
-                      restaurant={burger.restaurant[0].name}
-                    />
+                    <Link to={`/burgers/${burger._id}`}>
+                      <Card
+                        name={burger.name}
+                        image={burger.image}
+                        rating={burger.rating}
+                        restaurant={burger.restaurant[0].name}
+                      />
+                    </Link>
                   </div>
                 )}
               </div>
