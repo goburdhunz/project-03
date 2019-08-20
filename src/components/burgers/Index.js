37268a7e6749171cee4cdeb5b5b0f0b515b0f95e
camilabuenamar <camila.buenamar@gmail.com>
2019-08-20 10:00:17 +0100

    Ingredients filter modified

--- src/components/burgers/Index.js
+++ src/components/burgers/Index.js
@@ -8,13 +8,25 @@ import _ from 'lodash'
 const animatedComponents = makeAnimated()
 
 const ingredients = [
-  { value: 'bacon', label: 'Bacon' },
-  { value: 'beef', label: 'Beef' },
-  { value: 'tomato', label: 'Tomato' },
-  { value: 'lettuce', label: 'Lettuce' },
-  { value: 'cheese', label: 'Cheese' },
-  { value: 'pickles', label: 'Pickles' },
-  { value: 'ketchup', label: 'Ketchup' }
+  { value: 'Beef', label: 'Beef' },
+  { value: 'Chicken', label: 'Chicken' },
+  { value: 'Cheese', label: 'Cheese' },
+  { value: 'Tomato', label: 'Tomato' },
+  { value: 'Lettuce', label: 'Lettuce' },
+  { value: 'Gherkin', label: 'Gherkin' },
+  { value: 'Onion', label: 'Onion' },
+  { value: 'Jalapenos', label: 'Jalapeños' },
+  { value: 'Bacon', label: 'Bacon' },
+  { value: 'Steak', label: 'Steak' },
+  { value: 'Green pepper', label: 'Green pepper' },
+  { value: 'Mushroom', label: 'Mushroom' },
+  { value: 'Gravy', label: 'Gravy' },
+  { value: 'Bun', label: 'Bun' },
+  { value: 'Brioche', label: 'Brioche' },
+  { value: 'BBQ Sauce', label: 'BBQ Sauce' },
+  { value: 'Ketchup', label: 'Ketchup' },
+  { value: 'Mustard', label: 'Mustard' },
+  { value: 'Mayonnaise', label: 'Mayonnaise' }
 ]
 
 class BurgersIndex extends React.Component {
@@ -24,6 +36,7 @@ class BurgersIndex extends React.Component {
     this.state = {
       filterData: {
         searchTerm: '',
+        searchTermIng: '',
         sortTerm: 'price|asc',
         ingredients: []
       },
@@ -31,6 +44,7 @@ class BurgersIndex extends React.Component {
     },
     this.filterBurgers = this.filterBurgers.bind(this)
     this.handleKeyUp = this.handleKeyUp.bind(this)
+    this.handleKeyUpIngredients = this.handleKeyUpIngredients.bind(this)
     this.handleChangeOrder = this.handleChangeOrder.bind(this)
     this.handleChangeIngredients = this.handleChangeIngredients.bind(this)
     this.handleCheckbox = this.handleCheckbox.bind(this)
@@ -53,6 +67,11 @@ class BurgersIndex extends React.Component {
     this.setState({ filterData })
   }
 
+  handleKeyUpIngredients(e) {
+    const filterData = { ...this.state.filterData, searchTermIng: e.target.value }
+    this.setState({ filterData })
+  }
+
   handleCheckbox(e) {
     const filterData = { ...this.state.filterData, [e.target.name]: e.target.checked }
     this.setState({ filterData })
@@ -71,10 +90,11 @@ class BurgersIndex extends React.Component {
   filterBurgers() {
 
     const re = new RegExp(this.state.filterData.searchTerm, 'i')
+    const reIng = new RegExp(this.state.filterData.searchTermIng, 'i')
     const [field, order] = this.state.filterData.sortTerm.split('|')
     const filterBurgers = _.filter(this.state.burgers, burger => {
       return (this.state.filterData.ingredients.length ? _.intersection(burger.ingredients, this.state.filterData.ingredients).length : true) &&
-        (re.test(burger.ingredients) || re.test(burger.name) || re.test(burger.restaurant[0].name)) &&
+        (re.test(burger.name) || re.test(burger.restaurant[0].name)) && (reIng.test(burger.ingredients))  &&
         ((this.state.filterData.isVegan && burger.isVegan) ||
         (this.state.filterData.isVegetarian && burger.isVegetarian) ||
         (!this.state.filterData.isVegan && !this.state.filterData.isVegetarian))
@@ -115,6 +135,14 @@ class BurgersIndex extends React.Component {
                       onChange={this.handleChangeIngredients}
                       options={ingredients}
                     />
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        placeholder="Search for weird and wonderful ingredients"
+                        className="input"
+                        onKeyUp={this.handleKeyUpIngredients}/>
+                    </div>
                   </div>
                 </div>
 
