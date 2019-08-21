cd2d99f854603b1f48ac8d2dc546be8ccb7274b4
camilabuenamar <camila.buenamar@gmail.com>
2019-08-21 12:39:04 +0100

    fixed Show conflict

--- src/components/burgers/Show.js
+++ src/components/burgers/Show.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import Rating  from 'react-rating'
+import Rating from 'react-rating'
 ​
 import Comment from '../common/Comment'
 import Auth from '../../lib/Auth'
